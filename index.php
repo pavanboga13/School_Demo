49fa352e14d22f15bc00073eb1ed8343fc0c03d1
pavanboga13 <68605868+pavanboga13@users.noreply.github.com>
2021-02-01 22:08:19 +0530

    Demo check

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>School Demo</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="font/google font.css">
+	
+</head>
+<body>
+
+<?php 
+	include 'header.html';
+?>
+
+<br><br>
+
+<?php 
+	include 'header.html';
+?>
+
+<br><br>
+
+<?php 
+	include 'header.html';
+?>
+
+</body>
+</html>
